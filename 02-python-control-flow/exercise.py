527797ef5696b8eb54532a1b0334cdafd84bcb42
Eric Klimowich <41299438+eric-klimowich@users.noreply.github.com>
2022-07-11 19:18:07 -0400

    Update exercise.py

--- 02-python-control-flow/exercise.py
+++ 02-python-control-flow/exercise.py
@@ -4,7 +4,7 @@
 #     print("Clean the floor!")
 #     # more lines of code in this code block
 #     # need to be indented as well
-# #     print("This print is in the codeblock!")
+#     print("This print is in the codeblock!")
 # # print("This print is NOT IN THE CODEBLOCK!")
 # if walls == "sticky":
 #     print("Clean the walls!")
@@ -18,22 +18,14 @@
 # else:
 #     print("Strawberry milk for you")
 
-# color = ""
-# while color != 'quit':
-# # while True:
+
+
+
 #     color = input('Enter "green", "yellow", "red": ').lower()
 #     print(f'The user entered {color}')
 
-#     if color == "green":
-#         print("Go!")
-#     elif color == "yellow":
-#         print("Slow Down!")
-#     elif color == "red":
-#         print("Stop!")
-#     # elif color == "quit":
-#     #     break
-#     else:
-#         print("Bogus!")
+
+
 
 # names = ["Tom", "Deborah", "Murray", "Axel"]
 
@@ -49,10 +41,10 @@
 # for evenNum in range(2, 10, 2):
 #     print(evenNum)
 
-nums = list(range(10))
-print(nums)
-odds = tuple(range(1, 10, 2))
-print(odds)
+# nums = list(range(10))
+# print(nums)
+# odds = tuple(range(1, 10, 2))
+# print(odds)
 
-for num in range(5, 0, -1):
-  print(num)
+# for num in range(5, 0, -1):
+#   print(num)
